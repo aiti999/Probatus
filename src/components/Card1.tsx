0851d24e801580c86a14bfca9124a24a10d72f2a
aiti999 <110761811+aiti999@users.noreply.github.com>
2024-08-03 01:27:04 +0530

    <.>

--- src/components/Card1.tsx
+++ src/components/Card1.tsx
@@ -8,11 +8,11 @@ export type Card1Type = {
 const Card1: FunctionComponent<Card1Type> = ({ className = "", oval }) => {
   return (
     <div
-      className={`w-[514px] bg-white border-lavender-100 border-[1px] border-solid box-border shrink-0 flex flex-col items-start justify-start py-[30px] px-[31px] max-w-full text-left text-base text-dimgray-200 font-body mq800:w-[calc(100%_-_40px)] ${className}`}
+      className={`w-[514px] h-[400px] bg-white border-lavender-100 border-[1px] border-solid box-border shrink-0 flex flex-col items-start justify-start py-[30px] px-[31px] max-w-full text-left text-base text-dimgray-200 font-body mq800:w-[calc(100%_-_40px)] ${className}`}
     >
-      <div className="self-stretch flex flex-col items-start justify-start gap-8 gap-4">
+      <div className="self-stretch flex flex-col items-start justify-start gap-8">
         <img
-          className="w-24 h-4 relative"
+          className="w-24 h-4"
           loading="lazy"
           alt=""
           src="/favourite31.svg"
@@ -23,24 +23,25 @@ const Card1: FunctionComponent<Card1Type> = ({ className = "", oval }) => {
           retention and a 20% boost in app ratings. Client D's app is now a
           user-favorite, driving higher engagement and satisfaction.
         </div>
-        <div className="w-[236px] h-[54px] flex flex-row items-center justify-start gap-3.5 text-gray-900 font-franie">
-          <img
-            className="h-11 w-11 relative rounded-[50%] object-cover"
-            loading="lazy"
-            alt=""
-            src={oval}
-          />
-          <div className="flex-1 flex flex-col items-start justify-start gap-1">
-            <b className="self-stretch relative leading-[28px]">
-              Leslie Alexander
-            </b>
-            <div className="relative text-sm leading-[22px] font-body text-gray-600">
-              Freelance React Developer
-            </div>
-          </div>
+        <div className="w-[236px] h-[84px] flex items-center justify-start gap-2 text-gray-900 font-franie whitespace-nowrap">
+    <img
+        className="h-20 w-20 object-contain"
+        src={oval}
+    />
+    <div className="">
+        <b className="text-5xl font-bold">
+            Leslie Alexander
+        </b>
+        <div className="relative text-sm font-body text-gray-600">
+            Freelance React Developer
+        </div>
+    </div>
+</div>
+
+
         </div>
       </div>
-    </div>
+  
   );
 };
 
