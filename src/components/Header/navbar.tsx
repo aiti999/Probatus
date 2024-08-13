bb348ab5ba91eab55a1a33eaf54df31a7f7fde11
aiti999 <110761811+aiti999@users.noreply.github.com>
2024-08-13 14:53:56 +0530

    layout fix

--- src/components/Header/navbar.tsx
+++ src/components/Header/navbar.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent, useState } from "react";
 // import Hamburger from 'hamburger-react'
 import { GiHamburgerMenu } from "react-icons/gi";
 
@@ -7,25 +7,29 @@ export type NavbarProps = {
   className?: string;
 };
 
-const Navbar: FunctionComponent<NavbarProps> = ({ navigationItems, className = '' }) => {
+const Navbar: FunctionComponent<NavbarProps> = ({
+  navigationItems,
+  className = "",
+}) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
-    
-    
   };
 
   return (
-    <header className={`fixed top-0 left-1 right-12 py-5 px-12 z-50 bg-opacity-85 flex items-center justify-between ${className}`}>
-      <div><img
-        className="h-11 w-32 object-cover"
-        loading="lazy"
-        alt="Logo"
-        src="/artboard-5-2@2x.png"
-      />
+    <header
+      className={`fixed top-0 left-1 right-12 py-5 px-12 z-50 bg-opacity-85 flex items-center justify-between ${className}`}
+    >
+      <div>
+        <img
+          className="h-11 w-32 object-cover"
+          loading="lazy"
+          alt="Logo"
+          src="/artboard-5-2@2x.png"
+        />
       </div>
-      
+
       {/* Desktop Navigation */}
       <nav className="flex mq450:hidden flex-row items-center pr-12 justify-end gap-10 text-center font-semibold text-gray-300">
         {navigationItems.map((item) => (
@@ -36,45 +40,54 @@ const Navbar: FunctionComponent<NavbarProps> = ({ navigationItems, className = '
             {item.name}
           </a>
         ))}
-      <div>  <button className="cursor-pointer border border-solid bg-color-rgba(23, 23, 23, 1) border-gray-900 py-1.5 px-3 ml-44 bg-gray-800 rounded-lg flex flex-row items-center justify-center gap-2">
-          <a className="hover:border-neutral-600 no-underline relative text-sm leading-5 capitalize text-gray-200 inline-block" href="#book">
-            Book a Call
-          </a>
-          <div className="w-1.5 h-1.5 rounded-full bg-green-500" />
-        </button>
+        <div>
+          {" "}
+          <button className="cursor-pointer border border-solid bg-color-rgba(23, 23, 23, 1) border-gray-900 py-1.5 px-3 ml-44 bg-gray-800 rounded-lg flex flex-row items-center justify-center gap-2">
+            <a
+              className="hover:border-neutral-600 no-underline relative text-sm leading-5 capitalize text-gray-200 inline-block"
+              href="#book"
+            >
+              Book a Call
+            </a>
+            <div className="w-1.5 h-1.5 rounded-full bg-green-500" />
+          </button>
         </div>
       </nav>
 
       {/* Mobile Hamburger Button */}
-      <button className="ss:hidden text-white relative  bg-transparent z-50" onClick={toggleSidebar} aria-label="Toggle menu">
-
-        {/* <svg
-          className="w-6 h-6"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M4 6h16M4 12h16M4 18h16"
-          />
-        </svg> */}
-        <GiHamburgerMenu className='w-8 h-8' />
+      <button
+        className="ss:hidden text-white absolute right-3 bg-transparent z-50"
+        onClick={toggleSidebar}
+        aria-label="Toggle menu"
+      >
+        <GiHamburgerMenu className="w-7 h-7" />
       </button>
-      
+
       {/* Mobile Sidebar */}
-      <div className={`md:hidden fixed top-0 right-0 h-1/2 w-[27rem] bg-[url('/public/component-1@3x.png')]  transform ${isSidebarOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 ease-in-out z-40`}>
+      <div
+        className={`md:hidden fixed top-0 right-0 h-1/2 w-[27rem] bg-[url('/public/component-1@3x.png')]  transform ${
+          isSidebarOpen ? "translate-x-0" : "translate-x-full"
+        } transition-transform duration-300 ease-in-out z-40`}
+      >
         <div className="flex flex-col h-full p-4">
           <button
             className="self-end text-white absolute bg-black mb-8"
             onClick={toggleSidebar}
             aria-label="Close menu"
           >
-            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
             </svg>
           </button>
           <nav className="flex flex-col items-center justify-center relative top-24 gap-6 text-center font-semibold text-gray-300">
@@ -90,7 +103,10 @@ const Navbar: FunctionComponent<NavbarProps> = ({ navigationItems, className = '
           </nav>
           <div className="mt-auto">
             <button className="cursor-pointer border mb-20 ml-16 border-solid left-16 border-gray-900 py-1.5 px-1 bg-gray-800 rounded-lg flex flex-row items-center justify-center gap-1.5 w-2/3">
-              <a className="hover:border-neutral-600 relative text-sm leading-5 capitalize font-inter text-gray-200 inline-block" href="#book">
+              <a
+                className="hover:border-neutral-600 relative text-sm leading-5 capitalize font-inter text-gray-200 inline-block"
+                href="#book"
+              >
                 Book a Call
               </a>
               <div className="w-1.5 h-1.5 rounded-full bg-green-500" />
@@ -98,7 +114,7 @@ const Navbar: FunctionComponent<NavbarProps> = ({ navigationItems, className = '
           </div>
         </div>
       </div>
-      
+
       {/* Mobile Overlay */}
       {isSidebarOpen && (
         <div
--- src/components/HeroBackground.tsx
+++ src/components/HeroBackground.tsx
@@ -14,9 +14,9 @@ const HeroBackground: FunctionComponent<HeroBackgroundType> = ({
       <div className="w-[740px] flex flex-col items-start justify-start gap-3 max-w-full">
         <div className="self-stretch flex flex-row items-start justify-start relative max-w-full">
           <h1 className="m-0 flex-1 relative text-inherit inline-block max-w-full leading-[50px] font-inherit mq1400:text-6xl text-56px">
-            <span className="ss:text-[70px] text-[30px] font-thin">Unlocking Success</span>{" "}
+            <span className="ss:text-[70px] text-[48px] font-thin">Unlocking Success</span>{" "}
             <br />
-            <span className="ss:text-[70px] text-[30px]   font-thin">
+            <span className="ss:text-[70px] text-[48px]   font-thin">
               with <span className="font-bold text-[#ffffff]">Proven</span>,{" "}
               <span className="font-bold text-[#ffffff]">Tried</span> and{" "}
               <span className="font-bold text-[#ffffff]">Tested</span> Solutions
@@ -32,7 +32,7 @@ const HeroBackground: FunctionComponent<HeroBackgroundType> = ({
               SaaS Solutions
             </div>
             <div className="self-stretch h-11 flex flex-row items-start justify-center py-0 px-5 box-border">
-              <button className="cursor-pointer bg-green-500 hover:bg-green-400 border-darkgreen border-[1px] border-solid py-3 px-10 h-[46px] shadow-md rounded-xl flex flex-row items-center justify-center whitespace-nowrap">
+              <button className="cursor-pointer mq450:w-[350px] mq450:h-[56px] bg-green-500 hover:bg-green-400 border-darkgreen border-[1px] border-solid py-3 px-10 h-[46px] shadow-md rounded-xl flex flex-row items-center justify-center whitespace-nowrap">
                 <span className="text-base leading-5 text-[1rem] font-semibold text-gray-900">
                   Explore Our Services
                 </span>
