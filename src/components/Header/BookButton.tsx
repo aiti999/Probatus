6a2572f545dcaf2918495f84564e4deeb54962da
aiti999 <110761811+aiti999@users.noreply.github.com>
2024-08-23 00:11:21 +0530

    .

--- src/components/Header/BookButton.tsx
+++ src/components/Header/BookButton.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const BookButton: React.FC = () => (
-  <button className="cursor-pointer border border-solid bg-color-rgba(23, 23, 23, 1) border-gray-900 py-1.5 px-3 bg-[rgba(23,23,23,1)] rounded-lg flex items-center justify-center gap-2">
+  <button className="cursor-pointer border border-solid bg-color-rgba(23, 23, 23, 1) border-gray-900 py-1.5 px-3 bg-[rgba(23,23,23,1)] rounded-lg flex items-center justify-center gap-2 mq450:w-[21.5rem]">
     <a className="hover:border-neutral-600 no-underline relative text-sm leading-5 capitalize text-gray-200" href="#book">
       Book a Call
     </a>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,7 +11,7 @@ const navigationItems = [
 
 const Header: React.FC = () => {
   return (
-    <header className={`fixed top-0 left-0 right-0 self-stretch opacity-95 pt-8 overflow-hidden flex flex-row items-start justify-between py-5 px-20 md:px-10 lg:px-20 xl:px-30 bg-[url('/public/component-1@3x.png')] bg-cover bg-no-repeat bg-top z-50 max-w-full`}>
+    <header className={`fixed top-0 left-0 right-0 self-stretch opacity-96 pt-8 overflow-hidden flex flex-row items-start justify-between py-5 px-20 md:px-10 lg:px-20 xl:px-30 bg-[url('/public/component-1@3x.png')] bg-cover bg-no-repeat bg-top z-50 max-w-full`}>
       <div className='pt-4'>
         <Navbar navigationItems={navigationItems} />
       </div>
